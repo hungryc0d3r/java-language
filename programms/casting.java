c03858bdce0345fa491b74ba0d1ea5c46b0ab3c1
hungryc0d3r <shadowhunt2306@gmail.com>
2020-10-14 17:10:21 +0530

    casting

---
+++ programms/casting.java
@@ -0,0 +1,21 @@
+/**
+ * casting
+ */
+public class casting {
+   public static void main(String[] args) {
+    int myInt = 58256;
+    byte myByte = 82;
+    short myShort = 2586;
+    long myLong = 5283654;
+   //------------------- casting -----------------------
+    int myInt1 = (myInt/2);
+    short myShort1 = (short) (myShort/2);
+    byte myByte1 = (byte) (myByte/2);
+    long myLong1 = (myLong/2);
+     System.out.println(myInt1);
+     System.out.println(myByte1);
+     System.out.println(myShort1);
+     System.out.println(myLong1);
+   }
+
+}
