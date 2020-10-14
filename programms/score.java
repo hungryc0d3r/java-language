a34876d3911951a24c0514457be1b29425156c8b
hungryc0d3r <shadowhunt2306@gmail.com>
2020-10-15 00:36:51 +0530

    if and and operaator

---
+++ programms/score.java
@@ -0,0 +1,13 @@
+/**
+ * score
+ */
+public class score {
+
+  public static void main(String[] args) {
+    int myScore = 85;
+    if (myScore <100 && myScore>=8) {
+      System.out.println("First class");
+    }
+
+  }
+}
