90a679586616f703b449b142e2f9cfab4adfba03
hungryc0d3r <shadowhunt2306@gmail.com>
2020-10-14 16:17:56 +0530

    operators

---
+++ programms/operators.java
@@ -0,0 +1,12 @@
+public class operators{
+  public static void main(String[] args) {
+    int myFirstNum = 9;
+    int mySecondNum =8;
+
+      System.out.println(myFirstNum+mySecondNum);
+      System.out.println(myFirstNum-mySecondNum);
+      System.out.println(myFirstNum*mySecondNum);
+      System.out.println(myFirstNum/mySecondNum);
+      System.out.println(myFirstNum%mySecondNum);
+  }
+}
