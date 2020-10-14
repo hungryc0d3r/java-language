e1a64dd9a9242c033e866fac94f3b59a49e73440
hungryc0d3r <shadowhunt2306@gmail.com>
2020-10-14 18:08:39 +0530

    pounds to kilo

---
+++ programms/poundstokilo.java
@@ -0,0 +1,11 @@
+/**
+ * pounds to kilograms
+ */
+public class poundstokilo {
+
+  public static void main(String[] args) {
+    double noOfPounds = 200d;
+    double pounds2kilo = noOfPounds * 0.45359237d;
+    System.out.println("converted kilograms = " + pounds2kilo );
+  }
+}
