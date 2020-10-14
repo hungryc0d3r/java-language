a6c3c610d56dff167761cab7c4de95fd33396ccf
hungryc0d3r <shadowhunt2306@gmail.com>
2020-10-14 18:08:57 +0530

    if statement

---
+++ programms/statementif.java
@@ -0,0 +1,20 @@
+/**
+ * statementif | and | or
+ */
+public class statementif {
+
+  public static void main(String[] args) {
+    int topScore = 100;
+    int secTopScore = 80;
+    int score = 85;
+    if (score == 100) {
+      System.out.println("It is top score");
+    }
+
+    if (score < 100 && score >= 85) {
+      System.out.println("It is first grade");
+    }
+
+
+  }
+}
