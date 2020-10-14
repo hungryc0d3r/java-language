53b7f2cdff98f89b2a1ea1df6b2e6cd7cd8c04fc
hungryc0d3r <shadowhunt2306@gmail.com>
2020-10-14 17:31:26 +0530

    float double

---
+++ programms/primitivePart2.java
@@ -0,0 +1,17 @@
+/**
+ * float double
+*/
+public class primitivePart2 {
+  public static void main(String[] args) {
+    // float -> max and min value
+    float myMinFloat = Float.MIN_VALUE;
+    float myMaxFloat = Float.MAX_VALUE;
+    System.out.println(myMinFloat);
+    System.out.println(myMaxFloat);
+    // double -> max and min value
+    double myMinDouble = Double.MIN_VALUE;
+    double myMaxDouble = Double.MAX_VALUE;
+    System.out.println(myMinDouble);
+    System.out.println(myMaxDouble);
+  }
+}
